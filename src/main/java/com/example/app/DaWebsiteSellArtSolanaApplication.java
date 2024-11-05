65b43961ede9b21ce8cb25339c5bcb117057438f
leonx04 <jungnguyen2108@gmail.com>
2024-11-05 21:51:47 +0700

    upload code ver 1

---
+++ src/main/java/com/example/app/DaWebsiteSellArtSolanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaWebsiteSellArtSolanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaWebsiteSellArtSolanaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+
+    @GetMapping("/")
+    public String home() {
+
+        return "index.html";
+    }
+
+
+}
