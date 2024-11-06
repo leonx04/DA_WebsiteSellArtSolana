fe640f7fd5ac02fb4d6c8cc57e38d22fb201302f
Bachdung204 <bachdung004@gmail.com>
2024-11-06 10:13:04 +0700

    update

---
+++ src/main/java/com/example/app/model/user.java
@@ -0,0 +1,19 @@
+package com.example.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "chi_tiet_san_pham")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class user {
+}
